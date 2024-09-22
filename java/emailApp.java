711c732bf77d1d574bd2859dacba3d958824b1f9
Ritik Zambre <116812243+ritikZ18@users.noreply.github.com>
2024-09-22 12:00:07 -0700

    Create emailApp.java

---
+++ java/emailApp.java
@@ -0,0 +1,16 @@
+package emailApp;
+
+public class emailApp {
+
+    public static void main(String[] args) {
+
+        //constructor
+
+        email em1 = new email("Ritik", "Zambre");
+        System.out.println(em1.showInfo());
+//        em1.setAlternateEmail("js.es.@com");
+//        System.out.println(em1.getAlternateEmail());
+
+
+    }
+}
